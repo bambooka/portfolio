4736db70f83087370824c0498dab5a8c9e761aca
bambooka <sumkina.ol@gmail.com>
2020-10-08 14:04:27 +0300

    feat: add styles to project card

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -9,43 +9,48 @@ const Projects = () => {
     {
       name: 'Repo Search',
       image_url: 'https://docs.microsoft.com/learn/achievements/github/search-organize-repository-history-github-social.png',
-      description: ' Notice the use of %PUBLIC_URL% in the tags above.\n' +
-        '      It will be replaced with the URL of the `public` folder during the build.\n' +
-        '      Only files inside the `public` folder can be referenced from the HTML.',
+      description: 'Поиск репозиториев по заданным фильтрам. В проекте используется React, Redux (thunk)',
       github_url: 'https://github.com/bambooka',
       languages: ['java', 'js']
     },
     {
-      name: 'p 2',
+      name: 'ToDo List',
       image_url: 'https://cdn.dribbble.com/users/4920082/screenshots/10465675/02.png',
-      description: 'lorem',
+      description: 'Составление списка дел, их удаление и редактирование. В проекте используется React, Bootstrap ',
       github_url: 'https://github.com/bambooka',
       languages: ['java', 'js']
     },
     {
-      name: 'p 3',
+      name: 'Recipe App',
       image_url: 'https://docs.microsoft.com/learn/achievements/github/search-organize-repository-history-github-social.png',
-      description: 'lorem',
+      description: 'Поиск рецептов с сайта food2fork. В поректе используется React. Продеменстрирована работа со сторонним API',
       github_url: 'https://github.com/bambooka',
       languages: ['java', 'js']
     },
     {
-      name: 'p 4',
+      name: 'Tech Store',
       image_url: 'https://docs.microsoft.com/learn/achievements/github/search-organize-repository-history-github-social.png',
-      description: 'lorem',
+      description: 'Поиск и формирование заказа интернет-магазина электроники. В проекте используется React (styled.components, Context API), bootstrap',
       github_url: 'https://github.com/bambooka',
       languages: ['java', 'js']
     },
     {
-      name: 'p 5',
+      name: 'Restaurant Reviews',
       image_url: 'https://docs.microsoft.com/learn/achievements/github/search-organize-repository-history-github-social.png',
-      description: 'lorem',
+      description: 'Сервис для поиска ресторанов. В проекте ипродемонстрирована работа с service worker, indexed database, accesebility. На чистом Javascript ',
+      github_url: 'https://github.com/bambooka',
+      languages: ['java', 'js']
+    },
+    {
+      name: 'Fitbit Clockfaces',
+      image_url: 'https://docs.microsoft.com/learn/achievements/github/search-organize-repository-history-github-social.png',
+      description: 'Циферблаты для умных часов fitbit. В проекте использовался чистый Javascript (ES5/ES6), svg, websocket, обработка ответа сервера ',
       github_url: 'https://github.com/bambooka',
       languages: ['java', 'js']
     },
   ]
 
-  const [suggestions, setSuggestions] = useState([]);
+  const [setSuggestions] = useState([]);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -58,7 +63,7 @@ const Projects = () => {
     speed: 100,
     arrows: true,
     slidesToShow: 3,
-    slidesToScroll: 5
+    // slidesToScroll: 7
   }
   return (
     <>
@@ -71,49 +76,53 @@ const Projects = () => {
             </div>)
             : (<Slider {...settings}>
               {projectsData.map((current) => (
-                // <div className='project-card'>
-                //   <img className='project-image' src={current.image_url}/>
-                //   <div className='project-content'>
-                //     <h3>{current.name}</h3>
-                //     <div>ro
-                //       {current.languages.map(lang => <span>{lang}</span>)}
-                //     </div>
-                //     <p>{current.description}</p>
-                //   </div>
-                //   <div className='project-links'>
-                //     <a href={current.github_url}>github</a>
-                //     <a href={current.github_url}>project</a>
-                //   </div>
-                // </div>
-                <div className="out" key={current.name}>
+                <>
                   <div className="card">
-                    {/*<img*/}
-                    {/*  className="card-img-top"*/}
-                    {/*  alt={"users here"}*/}
-                    {/*  src={current.image_url}*/}
-                    {/*  height={56}*/}
-                    {/*  width={56}*/}
-                    {/*/>*/}
+
                     <div className="card-body">
                       <h5 className="card-title">{current.name}</h5>
-                      <span>Languages:
-                      {current.languages.map(lang => <span className='language'>{lang}</span>)}
-                      </span>
-                      <br/>
-                      <small className="card-text text-sm-center text-muted">
-                        {current.description}
-                      </small>
-                      <br/>
-                      <a className='card-link' href={current.github_url}>
-                        Github
+                      <p className="card-text">{current.description}</p>
+                    </div>
+                    <div className="card-footer links">
+                      <a className='card-link link' href='#'>
+                        <h6 className="text-muted">github</h6>
                       </a>
-                      <a className='card-link' href={current.github_url}>
-                        Project
+                      <a className='card-link link' href='#'>
+                        <h6 className="text-muted">project</h6>
                       </a>
-
                     </div>
                   </div>
-                </div>
+                  {/*<div className="out" key={current.name}>*/}
+                  {/*<div className="card">*/}
+                  {/*<img*/}
+                  {/*  className="card-img-top"*/}
+                  {/*  alt={"users here"}*/}
+                  {/*  src={current.image_url}*/}
+                  {/*  height={56}*/}
+                  {/*  width={56}*/}
+                  {/*/>*/}
+                  {/*<div className="card-body">*/}
+                  {/*  <h5 className="card-title">{current.name}</h5>*/}
+                  {/*  <span>Languages:*/}
+                  {/*    {current.languages.map(lang => <span className='language'>{lang}</span>)}*/}
+                  {/*  </span>*/}
+                  {/*  <br/>*/}
+                  {/*  <small className="card-text text-sm-center text-muted">*/}
+                  {/*    {current.description}*/}
+                  {/*  </small>*/}
+                  {/*  <br/>*/}
+                  {/*  <div className='card-footer'>*/}
+                  {/*    <a className='card-link' href={current.github_url}>*/}
+                  {/*      <small className='text-muted'>Github</small>*/}
+                  {/*    </a>*/}
+                  {/*    <a className='card-link' href={current.github_url}>*/}
+                  {/*      <small className='text-muted'>Project</small>*/}
+                  {/*    </a>*/}
+                  {/*  </div>*/}
+                  {/*</div>*/}
+                  {/*</div>*/}
+                  {/*</div>*/}
+                </>
               ))}
             </Slider>)
         }
