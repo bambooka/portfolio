e1c1aa19ae5373259b7472da0a0f1b7254c00196
bambooka <sumkina.ol@gmail.com>
2020-10-10 20:20:38 +0300

    feat: add primary color

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 import {Route, Switch} from 'react-router-dom';
